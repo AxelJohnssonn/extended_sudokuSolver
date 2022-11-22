2b8480d0ba6c357b68f7d386a88470ef15744acd
Axel Johnsson <mr.axjo3@gmail.com>
2022-11-22 16:48:20 +0100

    .

--- Sudoku_JAVA/Sudoku.java
+++ Sudoku_JAVA/Sudoku.java
@@ -1,52 +1,72 @@
+package Sudoku_JAVA;
 public class Sudoku implements SudokuSolver{
     private int[][] board;
 
     public Sudoku() {
-        //Testar endast med 3x3
-        this.board = new int[3][3];
+        this.board = new int[9][9];
     }
 
     /**
      * Check if it is legal to place value at row, col.
      * @return true if value can be placed at row, col, false otherwise
      */
-    boolean checkIfLegal(int row, int col, int value);
+    public boolean checkIfLegal(int row, int col, int value){
+
+    }
 
     /**
      * Check if the position at row, col has a start value
      * @return true if the position has no start value, false otherwise
      */
-    boolean checkIfEmpty(int row, int col);
+    public boolean checkIfEmpty(int row, int col){
+
+    }
 
     /**
      * Initializes the board with values in the matrix start.
      */
-    void init(int[][] start);
+    public void init(int[][] start){
+        for(int r=0; r<=9; r++){
+            for(int c=0; c<=9; c++){
+                board[r][c] =start[r][c]; 
+            }
+        }
+    }
     /**
      * Returns the solution.
      * @return int matrix with a valid solution
      */
-    int[][] getBoard();
+    public int[][] getBoard(){
+        return board; 
+    }
     /**
      * Method to solve the sudoku.
      * @return true if solution was found, false otherwise
      */
-    boolean solve(int row, int col); //solve(0, 0)
+    public boolean solve(int row, int col){
+
+    } //solve(0, 0)
 
     /**
      * Adds value value at position row, col.
      */
-    void add(int row, int col, int value);
+    public void add(int row, int col, int value){
+
+    }
 
     /**
      * Clears the sudoku.
      */
-    void clear();
+    public void clear(){
+
+    }
 
     /**
      * Removes the value at row, col.
      */
-    void remove(int row, int col);
+    public void remove(int row, int col){
+        
+    }
 
     
 }
