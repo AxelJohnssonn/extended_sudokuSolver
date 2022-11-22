c21e27333b0ca1989b823cd8cc6b7da1afe14551
Axel Johnsson <mr.axjo3@gmail.com>
2022-11-22 17:17:52 +0100

    .

--- Sudoku_JAVA/Sudoku.java
+++ Sudoku_JAVA/Sudoku.java
@@ -11,6 +11,22 @@ public class Sudoku implements SudokuSolver{
      * @return true if value can be placed at row, col, false otherwise
      */
     public boolean checkIfLegal(int row, int col, int value){
+
+        //checkar columner
+        for(int r=0; r<9; r++){
+           if(row != r && board[r][col] == value){
+            return false;
+           }
+        }
+        //checkar rader
+        for(int c=0; c<9; c++){
+            if(col != c && board[row][c] == value){
+                return false; 
+            }
+        }
+
+        //checkar inom varje 3x3 HUR FAN GÖR JAG DET!?!?!?!
+
         return true;
     }
 
@@ -19,7 +35,12 @@ public class Sudoku implements SudokuSolver{
      * @return true if the position has no start value, false otherwise
      */
     public boolean checkIfEmpty(int row, int col){
-        return true;
+
+        if(board[row][col] == 0){
+            return true; 
+        }
+
+        return false;
     }
 
     /**
@@ -28,7 +49,7 @@ public class Sudoku implements SudokuSolver{
     public void init(int[][] start){
         for(int r=0; r<9; r++){
             for(int c=0; c<9; c++){
-                board[r][c] =start[r][c]; 
+                board[r][c] = start[r][c]; 
             }
         }
     }
@@ -51,21 +72,25 @@ public class Sudoku implements SudokuSolver{
      * Adds value value at position row, col.
      */
     public void add(int row, int col, int value){
-
+        board[row][col] = value; 
     }
 
     /**
      * Clears the sudoku.
      */
     public void clear(){
-
+        for(int r=0; r<9; r++){
+            for(int c=0; c<9; c++){
+                board[r][c] = 0; 
+            }
+        }
     }
 
     /**
      * Removes the value at row, col.
      */
     public void remove(int row, int col){
-        
+        board[row][col] = 0; 
     }
 
     
