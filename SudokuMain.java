a13988aa20f0e3ad4ba7242ed3ceab4f76d2ce4b
Axel Johnsson <mr.axjo3@gmail.com>
2022-11-22 16:11:35 +0100

    .

---
+++ SudokuMain.java
@@ -0,0 +1,5 @@
+public class SudokuMain{
+    public static void main(String[] args) {
+        
+    }
+}
--- SudokuSolver.java
+++ SudokuSolver.java
@@ -26,7 +26,8 @@ public interface SudokuSolver{
      * Method to solve the sudoku.
      * @return true if solution was found, false otherwise
      */
-    boolean solve(int row, int col); //solve(0, 0)
+    boolean solve(int row, int col){                                 
+    } //solve(0, 0)
 
     /**
      * Adds value value at position row, col.
