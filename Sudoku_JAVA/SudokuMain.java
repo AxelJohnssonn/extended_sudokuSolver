4d96624f3a57386f22dd7f7840d58bb456a14756
DESKTOP-OSFSGV9\Jeppe <mosaren@hotmail.com>
2022-11-23 22:03:30 +0100

    cleaned up main

--- Sudoku_JAVA/SudokuMain.java
+++ Sudoku_JAVA/SudokuMain.java
@@ -13,16 +13,6 @@ public class SudokuMain{
 
         SudokuView b1 = new SudokuView(game);
 
-        int[][] newBoard = new int[9][9];
-        newBoard[2][2] = 4;
-        newBoard[1][2] = 4;
-        newBoard[2][1] = 4;
-        
-        Sudoku game2 = new Sudoku(); 
-        game2.init(newBoard);
-
-        b1.setOutputBoard(newBoard);
-
         for(int r=0; r<9; r++){
             for(int c=0; c<9; c++){
                 System.out.print(board[1][c]); 
